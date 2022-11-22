797b7bad4713c99bb9fbf1c10da378454b71c8e9
unothedog <welrodriguez2022@gmail.com>
2022-11-23 00:02:41 +0800

    modified:   Assignment and Activity Repetition Control Structures/script.js

--- Assignment and Activity Repetition Control Structures/script.js
+++ Assignment and Activity Repetition Control Structures/script.js
@@ -1,17 +1,5 @@
 
-/* 
-Create a JavaScript code that will determine if the input type is even or odd.
-
-MAIN GOAL:
-Perform a Nested If for this scenario.
-Enclose inside the function.
-When a user input a word, it will display “Unexpected Input”
-When a user inputs none, it will display “Please input inside the box”.
-When a user inputs a valid number, it will display even or odd.
-
-
-
-*/
+console.log("connected")
 
 
 
