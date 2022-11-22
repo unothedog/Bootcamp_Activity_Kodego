3a161eb08f25630afac2fec50e303f767ac232cd
unothedog <welrodriguez2022@gmail.com>
2022-11-22 23:58:19 +0800

    modified:   Assignment and Activity Repetition Control Structures/script.js

--- Assignment and Activity Repetition Control Structures/script.js
+++ Assignment and Activity Repetition Control Structures/script.js
@@ -15,5 +15,30 @@ When a user inputs a valid number, it will display even or odd.
 
 
 
+ function oddEven(num){
+    if(isNaN (num) || typeof parseInt (num) !== "number"){
+        //isNan - determines whether a values is NaN (not a number) or a number
+        return `Unexpected Input`;
+     }
+
+    else if (num === '') {
+        return "Please input inside the box";
+   }
+
+    else {
+        if (num % 2 == 0)
+       {
+            return ("Even Number");
+        }
+        else {
+            return ("Odd Number");
+        }
+    }
+ }
+
+ let userInput = prompt ("Give Me a Number");
+console.log(oddEven(userInput));
+
+
 
 
