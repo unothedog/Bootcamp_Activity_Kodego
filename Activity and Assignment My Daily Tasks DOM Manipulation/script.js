3135cb9ae2b64ea1a2cf928afe92fa54c5d6a44f
unothedog <welrodriguez2022@gmail.com>
2022-11-24 23:17:43 +0800

    new file:   Activity and Assignment My Daily Tasks DOM Manipulation/index.html 	new file:   Activity and Assignment My Daily Tasks DOM Manipulation/script.js 	new file:   JS practice code/array.html 	new file:   JS practice code/array_answer.js 	new file:   JS practice code/index.html 	new file:   JS practice code/script.js 	new file:   s20/login.html 	new file:   s20/login.js 	new file:   s20/task.html 	new file:   s20/task.js
    Untracked files:
    	media query/recreate_html_forms/

---
+++ Activity and Assignment My Daily Tasks DOM Manipulation/script.js
@@ -0,0 +1,48 @@
+
+console.log("connected");
+const add = document.querySelector("#add");
+const todoWrapper = document.querySelector(".todoWrapper");
+
+
+add.addEventListener ("click", () => {
+    let userInput = document.querySelector("#userInput").value;
+
+    if(userInput.length > 5)
+    {
+        let li = document.createElement('li');
+        li.className = "form-check mb-2"
+        li.id = "check"
+
+        let checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.id = "todo-list"
+
+        li.appendChild(document.createTextNode(userInput));
+        
+              
+
+        todoWrapper.appendChild(li)
+        li.appendChild(checkbox);
+        
+
+        document.querySelector("#userInput").value="";
+    }
+
+    else
+    {
+        alert("You must enter more than 5 characters");
+    }
+});
+
+
+const remove = document.querySelector("#delete");
+remove.addEventListener ("click", () =>{
+    let checkBoxes = document.querySelectorAll('input[type="checkbox"]')
+    let li = document.getElementById("check");
+    for(let item of checkBoxes){
+        if (item.checked) { 
+            item.parentElement.remove(li)
+        }
+    } 
+});
+
---
+++ s20/task.js
@@ -0,0 +1,9 @@
+console.log("connected");
+
+//Get Access to the button
+let button = document.querySelector("#button");
+
+//querySelector method - returns the first element that matches a CSS selector.
+
+//Get Access to the ul tag (parent tag of li tag)
+let todoWrapper = document.querySelector(".todo-wrapper");
